06d2b0b12d024c865199ef3f56c70dfa5496e9da
Oleksii Baranovskyi <c0ldsh0t1718@gmail.com>
2026-07-15 21:18:23 +0200

    Add initial seed data for artists and genres

---
+++ backend/database/insert_data.sql
@@ -0,0 +1,37 @@
+
+-- =====================================================
+-- Artists
+-- =====================================================
+
+INSERT INTO Artists (
+    Name,
+    Country,
+    ExternalSource,
+    ExternalId
+    )
+   VALUES 
+    ('Linkin Park','USA',NULL,NULL),
+    ('Imagine Dragons', 'USA', NULL, NULL),
+    ('Eminem', 'USA', NULL, NULL),
+    ('Queen', 'United Kingdom', NULL, NULL),
+    ('The Weeknd', 'Canada', NULL, NULL);
+
+-- =====================================================
+-- Genres
+-- =====================================================
+
+
+INSERT INTO Genres (
+    Name)
+VALUES 
+('Rock'),
+('Alternative Rock'),
+('Pop Rock'),
+('Hard Rock'),
+('Nu Metal'),
+('Hip Hop'),
+('R&B'),
+('Pop'),
+('Electronic'),
+('Metal');
+
